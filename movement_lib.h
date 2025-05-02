e02c4752e0a9a2c408a59b3d09c2b5a1ea25d7ed
Sareeser <156449128+Sareeser@users.noreply.github.com>
2025-05-02 10:11:06 +0200

    Add files via upload

---
+++ movement_lib.h
@@ -0,0 +1,8 @@
+void straigth(int dist);
+void turn_right(int angle);
+void turn_left(int angle);
+void twist(int angle);
+void strave_right(int angle, int dist);
+void strave_left(int angle, int dist);
+
+void dance(void);
