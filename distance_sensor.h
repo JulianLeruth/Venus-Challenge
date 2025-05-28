3d8b6ae2c66cf95a3ecc2fb4925061cc4f9e140b
Sareeser <156449128+Sareeser@users.noreply.github.com>
2025-05-29 01:32:25 +0200

    Update distance_sensor.h

--- distance_sensor.h
+++ distance_sensor.h
@@ -1,1 +1,5 @@
+int flushIICChannel(vl53x* sensor, int number_of_dist_measurements);
+int vl53l0xPing(vl53x* sensor);
+int vl53l0xFlush(vl53x* sensor);
 int vl53l0xExample(void);
+int vl53l0xTest(void);
