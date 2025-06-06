5df4321fdb09534af1b8b4b13c407405455334ad
JulianLeruth <julian.leruth@outlook.com>
2025-06-06 09:36:41 +0200

    Update IR_test.c

--- IR_test.c
+++ IR_test.c
@@ -8,12 +8,16 @@ int main(void){
 
 
     switchbox_set_pin(IO_A0, SWB_GPIO);
-
+    switchbox_set_pin(IO_A1, SWB_GPIO);
+    
     gpio_set_direction(IO_A0, GPIO_DIR_INPUT);
+    gpio_set_direction(IO_A1, GPIO_DIR_INPUT);
 
     while(1){
-        gpio_level_t c = gpio_get_level(IO_A0);
-        printf("Message: %d\n", c);
+        gpio_level_t c0 = gpio_get_level(IO_A0);
+        gpio_level_t c1 = gpio_get_level(IO_A1);
+        printf("Message 0: %d\n", c0);
+        printf("Message 1: %d\n", c1);
         sleep_msec(25);
     }
 
