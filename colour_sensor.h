67eff5194201363ab8eb2df3013deab7902e52d0
Sareeser <156449128+Sareeser@users.noreply.github.com>
2025-06-11 17:13:21 +0200

    Create colour_sensor.h

---
+++ colour_sensor.h
@@ -0,0 +1,3 @@
+int tcs3472Ping(tcs3472* sensor, int integration_time_ms);
+void printColour(uint16_t red, uint16_t green, uint16_t blue);
+int colourSensor(tcs3472* sensor, int integration_time_ms);
