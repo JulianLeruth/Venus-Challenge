b57d777120e029c6de33e7022cb2a12840fccebc
Sareeser <156449128+Sareeser@users.noreply.github.com>
2025-06-11 17:16:39 +0200

    Update movement_lib.c

--- movement_lib.c
+++ movement_lib.c
@@ -24,15 +24,15 @@
     Avoid using one or two letter variables ("velocity" instead of "v")
                                                                         */
 
-#define PI 3.14159265358979323846
-#define ROBOT_WIDTH 12.3
-#define STRAIGTH_STEPS_IN_CM 64                     // Max number of steps ~= 32767
-#define NUMBER_OF_MEASUREMENT_FOR_TURNING 120
-#define NUMBER_OF_DIST_MEASUREMENT_PER_SECOND 20
+#define PI                  3.14159265358979323846
+#define ROBOT_WIDTH                           12.3
+#define STRAIGTH_STEPS_IN_CM                    64          // Max number of steps ~= 32767
+#define NUMBER_OF_MEASUREMENT_FOR_TURNING_360  120
+#define NUMBER_OF_DIST_MEASUREMENT_PER_SECOND   20
 
-#define SMALL_BLOCK_MIN_NUM_MEASUREMENTS 5          // The minimum number of measurements it should take for the robot to detect a small block.
-#define SMALL_BLOCK_MAX_NUM_MEASUREMENTS 15         // The maximum number of measurements it can take for the robot to detect a small block and the minimum for the robot to detect a big block.
-#define BIG_BLOCK_MAX_NUM_MEASUREMENTS 25           // The maximum number of measurements it can take for the robot to detect a big block.
+#define SMALL_BLOCK_MIN_NUM_MEASUREMENTS         5          // The minimum number of measurements it should take for the robot to detect a small block.
+#define SMALL_BLOCK_MAX_NUM_MEASUREMENTS        15          // The maximum number of measurements it can take for the robot to detect a small block and the minimum for the robot to detect a big block.
+#define BIG_BLOCK_MAX_NUM_MEASUREMENTS          25          // The maximum number of measurements it can take for the robot to detect a big block.
 
 
 // ===================== Basis Value Functions ====================== //
@@ -46,7 +46,7 @@ int sizeOfArray(int* arr) {
 /* The 'highestValueIndex' function checks the given array and returns the index of the highest number. */  
 int highestValueIndex(int* arr) {
     int index = 0;
-    for (int i = 1; i < NUMBER_OF_MEASUREMENT_FOR_TURNING; i++) {
+    for (int i = 1; i < sizeOfArray(arr); i++) {
         if (arr[0] == 0) {
         index = 1;
         i++;
@@ -59,10 +59,10 @@ int highestValueIndex(int* arr) {
 /* The 'lowestValueIndex' function checks the given array and returns the index of the lowest number. */
 int lowestValueIndex(int* arr) {
     int index = 0;
-    for (int i = 1; i < NUMBER_OF_MEASUREMENT_FOR_TURNING; i++) {
+    for (int i = 1; i < sizeOfArray(arr); i++) {
         if (arr[0] <= 100) {
-        index = 1;
-        i++;
+            index = 1;
+            i++;
         }
         if (arr[i] < arr[index] && arr[i] > 50) index = i;
     }
@@ -71,7 +71,10 @@ int lowestValueIndex(int* arr) {
 
 /* The 'convoluteArr' function takes an average of the value before, the value of and the value after an index of the array. */
 void convoluteArr(int* arr) {
-    for(int i = 1; i < NUMBER_OF_MEASUREMENT_FOR_TURNING-1; i++) {
+    if (arr[0] <= 20) arr[0] = 8190;
+    if (arr[1] <= 20) arr[1] = 8190;
+    for(int i = 1; i < sizeOfArray(arr)-1; i++) {
+        if (arr[i+1] <= 20) arr[i+1] = 8190;
         arr[i] = (arr[i-1]+arr[i]+arr[i+1])/3;
     }
 }
@@ -200,22 +203,22 @@ void dance(void){
 
 // =========== Advanced Movement and detection functions ============ //
 /* The 'sizeDetection' function measures the size of an object and returns either 1, 2, 3 based on which size the object is */
-int sizeDetection(vl53x sensor) {
+int sizeDetection(vl53x* sensor) {
     int32_t iDistance;
     int32_t prevDistance;
     int number_of_measurements = 2;
     int number_of_dist_measurements = 0;
-    iDistance = tofReadDistance(&sensor);
+    iDistance = tofReadDistance(sensor);
     do {
         prevDistance = iDistance;
-        iDistance = tofReadDistance(&sensor);
+        iDistance = tofReadDistance(sensor);
         printf("Left: Distance = %dmm\n", iDistance);
         stepper_steps(-16, 16);
         waitTillDone();
         number_of_dist_measurements++;
-        int temp = flushIICChannel(&sensor, number_of_dist_measurements);
-        if (temp == -1) return -1;
-        if (temp == 1) number_of_dist_measurements = 0;
+        int temp = flushIICChannel(sensor, number_of_dist_measurements);
+        if (temp == EXIT_FAILURE) return -1;
+        if (temp == EXIT_SUCCESS) number_of_dist_measurements = 0;
     } while(iDistance - prevDistance < 100);
     
     stepper_disable();
@@ -225,22 +228,22 @@ int sizeDetection(vl53x sensor) {
     sleep_msec(500);
     stepper_steps(32, -32);
     sleep_msec(500);
-    iDistance = tofReadDistance(&sensor);
+    iDistance = tofReadDistance(sensor);
     printf("Corner Found!\nDistance = %dmm\n", iDistance);
     stepper_steps(32, -32);
     waitTillDone();
     
     do {
         prevDistance = iDistance;
-        iDistance = tofReadDistance(&sensor);
+        iDistance = tofReadDistance(sensor);
         printf("Right: Distance = %dmm\n", iDistance);
         stepper_steps(16, -16);
         waitTillDone();
         number_of_measurements++;
         number_of_dist_measurements++;
-        int temp = flushIICChannel(&sensor, number_of_dist_measurements);
-        if (temp == -1) return -1;
-        if (temp == 1) number_of_dist_measurements = 0;
+        int temp = flushIICChannel(sensor, number_of_dist_measurements);
+        if (temp == EXIT_FAILURE) return -1;
+        if (temp == EXIT_SUCCESS) number_of_dist_measurements = 0;
     } while(iDistance < 500 && iDistance - prevDistance < 100);
     
     motorReset();
@@ -251,58 +254,57 @@ int sizeDetection(vl53x sensor) {
     printf("Flushed\n");
     iic_destroy(IIC0);
     iic_init(IIC0);
-    if (vl53l0xFlush(&sensor) == -1) return -1;
+    if (vl53l0xFlush(sensor) == -1) return -1;
 
     if (number_of_measurements >= SMALL_BLOCK_MIN_NUM_MEASUREMENTS
         && number_of_measurements <= SMALL_BLOCK_MAX_NUM_MEASUREMENTS)  return 1;
     else if (number_of_measurements > SMALL_BLOCK_MIN_NUM_MEASUREMENTS
         && number_of_measurements <= BIG_BLOCK_MAX_NUM_MEASUREMENTS)    return 2;
     else if (number_of_measurements > BIG_BLOCK_MAX_NUM_MEASUREMENTS)   return 3;
-    return 0;
+    else return 0;
 }
 
-/* The 'objectDectection' function make a 360 degree turn and detects the closest objects to the robot and then turns back to the closest object */
-int objectDetectionTwist(vl53x sensor) {
+/* The 'objectDectection360' function make a 360 degree turn and detects the closest objects to the robot and then turns back to the closest object */
+int objectDetectionTwist360(vl53x* sensor) {
     /* === Initiation === */
     stepper_reset();
     stepper_enable();
 
-    int32_t iDistanceArr[NUMBER_OF_MEASUREMENT_FOR_TURNING+1];
-    int loop = 0;
+    int32_t iDistanceArr[NUMBER_OF_MEASUREMENT_FOR_TURNING_360+1];
     int number_of_dist_measurements = 0;
 
     /* === First measurement turning circle === */
     twist(-360, 63000);
-    for(int i = 0; i < NUMBER_OF_MEASUREMENT_FOR_TURNING; i++) {
-        iDistanceArr[i] =  tofReadDistance(&sensor);
+    for(int i = 0; i < NUMBER_OF_MEASUREMENT_FOR_TURNING_360; i++) {
+        iDistanceArr[i] =  tofReadDistance(sensor);
         printf("%d: Distance = %dmm\n", i, iDistanceArr[i]);
         sleep_msec(50);
         number_of_dist_measurements++;
-        int temp = flushIICChannel(&sensor, number_of_dist_measurements);
-        if (temp == -1) return -1;
-        if (temp == 1) number_of_dist_measurements = 0;
+        int temp = flushIICChannel(sensor, number_of_dist_measurements);
+        if (temp == EXIT_FAILURE) return EXIT_FAILURE;
+        if (temp == EXIT_SUCCESS) number_of_dist_measurements = 0;
     }
     waitTillDone();
 
     /* === Lowest value of the measurements === */
     printf("\nArr: [%d", iDistanceArr[0]);
-    for(int i = 1; i < NUMBER_OF_MEASUREMENT_FOR_TURNING; i++) printf(", %d", iDistanceArr[i]);
+    for(int i = 1; i < NUMBER_OF_MEASUREMENT_FOR_TURNING_360; i++) printf(", %d", iDistanceArr[i]);
     printf("]\n");
     
     /* === Convolution of the measurements === */
     convoluteArr(iDistanceArr);
     int lowest_distance_index = lowestValueIndex(iDistanceArr);
-    printf("\n\033[0;31mlowest distance index: \033[0;37m%d\n\033[0;31mlowest distance: \033[0;37m%d mm \n\nArr: [%d", lowest_distance_index, iDistanceArr[lowest_distance_index], iDistanceArr[0]);
-    for(int i = 1; i < NUMBER_OF_MEASUREMENT_FOR_TURNING; i++) printf(", %d", iDistanceArr[i]);
+    printf("\n\033[31mlowest distance index: \033[0m%d\n\033[31mlowest distance: \033[0m%d mm \n\nArr: [%d", lowest_distance_index, iDistanceArr[lowest_distance_index], iDistanceArr[0]);
+    for(int i = 1; i < NUMBER_OF_MEASUREMENT_FOR_TURNING_360; i++) printf(", %d", iDistanceArr[i]);
     printf("]\n");
     
     
     /* === Turn back to the lowest value === */
-    int turningAngle = -360*lowest_distance_index/NUMBER_OF_MEASUREMENT_FOR_TURNING;
+    int turningAngle = -360*lowest_distance_index/NUMBER_OF_MEASUREMENT_FOR_TURNING_360;
     if (turningAngle > 180) turningAngle -= 360;
     else if (turningAngle < -180) turningAngle += 360;
 
-    printf("\033[0;32m\nTurning %d degrees!\n\n\033[0;37m", turningAngle);
+    printf("\033[32m\nTurning %d degrees!\n\n\033[0m", turningAngle);
     twist(turningAngle, 48000);
     waitTillDone();
 
@@ -310,8 +312,92 @@ int objectDetectionTwist(vl53x sensor) {
     printf("Flushed\n");
     iic_destroy(IIC0);
     iic_init(IIC0);
-    if (vl53l0xFlush(&sensor) == -1) return -1;
 
-    if (loop == NUMBER_OF_MEASUREMENT_FOR_TURNING) return 0;
-    else return 1;
+    if (vl53l0xFlush(sensor) == EXIT_FAILURE) return EXIT_FAILURE;
+    else return EXIT_SUCCESS;
+}
+
+/* The 'objectDectection180' function make a 180 degree turn and detects the closest objects to the robot and then turns back to the closest object */
+int objectDetectionTwistPart(vl53x* sensor, int angle) {
+    /* === Initiation === */
+    stepper_reset();
+    stepper_enable();
+    int number_of_measurements = (float)(NUMBER_OF_MEASUREMENT_FOR_TURNING_360*angle)/360.0;
+
+    int32_t iDistanceArr[angle+1];
+    int number_of_dist_measurements = 0;
+    twist(angle/2, 30000);
+    waitTillDone();
+
+    /* === First measurement turning circle === */
+    twist(-angle, 63000);
+
+    for(int i = 0; i < number_of_measurements; i++) {
+        iDistanceArr[i] =  tofReadDistance(sensor);
+        printf("%d: Distance = %dmm\n", i, iDistanceArr[i]);
+        sleep_msec(50);
+        number_of_dist_measurements++;
+        int temp = flushIICChannel(sensor, number_of_dist_measurements);
+        if (temp == EXIT_FAILURE) return EXIT_FAILURE;
+        if (temp == EXIT_SUCCESS) number_of_dist_measurements = 0;
+    }
+    waitTillDone();
+
+    /* === Lowest value of the measurements === */
+    printf("\nArr[%d]: [%d", sizeOfArray(iDistanceArr), iDistanceArr[0]);
+    for(int i = 1; i < number_of_measurements; i++) printf(", %d", iDistanceArr[i]);
+    printf("]\n");
+    
+    /* === Convolution of the measurements === */
+    convoluteArr(iDistanceArr);
+    int lowest_distance_index = lowestValueIndex(iDistanceArr);
+    printf("\n\033[31mlowest distance index: \033[0m%d\n\033[31mlowest distance: \033[0m%d mm \n\nConvoluted Arr: [%d", lowest_distance_index, iDistanceArr[lowest_distance_index], iDistanceArr[0]);
+    for(int i = 1; i < number_of_measurements; i++) {
+        if (i == lowest_distance_index) {
+            printf(", \033[35m%d\033[0m", iDistanceArr[i]);
+        }
+        else printf(", %d", iDistanceArr[i]);
+    }
+    printf("]\n");
+    
+    
+    /* === Turn back to the lowest value === */
+    int turningAngle = angle*1.05*(number_of_measurements - lowest_distance_index)/number_of_measurements;
+
+    printf("\033[32m\nTurning %d degrees!\n\n\033[0m", turningAngle);
+    twist(turningAngle, 48000);
+    waitTillDone();
+
+    /* === Safety Flush === */
+    printf("Flushed\n");
+    iic_destroy(IIC0);
+    iic_init(IIC0);
+
+    if (vl53l0xFlush(sensor) == EXIT_FAILURE) return EXIT_FAILURE;
+    else return EXIT_SUCCESS;
+}
+
+/* The 'objectAproach' function aproaches an object and stops if the object is closer than the a given distance */
+int objectAproach(vl53x* sensor, int max_dist) {
+    stepper_reset();
+    stepper_enable();
+    
+    int number_of_dist_measurements = 0;
+	int32_t iDistance;
+	int32_t prev_distance;
+    
+    stepper_set_speed(45000, 45000);
+    iDistance = tofReadDistance(sensor);
+    do {
+        prev_distance = iDistance;
+        iDistance = tofReadDistance(sensor);
+        printf("Distance = %dmm\n", iDistance);
+        stepper_steps(64, 64);
+        sleep_msec(50);
+        number_of_dist_measurements++;
+        int temp = flushIICChannel(sensor, number_of_dist_measurements);
+        if (temp == EXIT_FAILURE) return EXIT_FAILURE;
+        if (temp == EXIT_SUCCESS) number_of_dist_measurements = 0;
+    } while(iDistance > max_dist || prev_distance > max_dist);
+    return EXIT_SUCCESS;
 }
