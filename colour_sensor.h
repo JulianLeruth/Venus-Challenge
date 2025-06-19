00a155fea6cd29b5b0c1f2d25d3501f28c00f948
Sareeser <156449128+Sareeser@users.noreply.github.com>
2025-06-19 17:38:21 +0200

    Update colour_sensor.h

--- colour_sensor.h
+++ colour_sensor.h
@@ -1,3 +1,11 @@
-int tcs3472Ping(tcs3472* sensor, int integration_time_ms);
-void printColour(uint16_t red, uint16_t green, uint16_t blue);
-int colourSensor(tcs3472* sensor, int integration_time_ms);
+#define BLACK                               0
+#define WHITE                               1
+#define RED                                 2
+#define BLUE                                3
+#define GREEN                               4
+
+extern int tcs3472Ping(tcs3472* sensor, int integration_time_ms);
+
+extern void printColour(uint16_t red, uint16_t green, uint16_t blue);
+
+extern int colourSensor(tcs3472* sensor, int integration_time_ms);
