5068aa4d547ef69060026c9a83aa5c5828f92c38
Sareeser <156449128+Sareeser@users.noreply.github.com>
2025-06-20 02:43:11 +0200

    Update infrared_sensor.c

--- infrared_sensor.c
+++ infrared_sensor.c
@@ -2,24 +2,19 @@
 #include <stdio.h>
 #include <switchbox.h>
 
+#include "infrared_sensor.h"
+
 int IRborderDetection() {
-    gpio_init();
-
-
-    gpio_set_direction(IO_AR4, GPIO_DIR_INPUT);
-    int rightside = gpio_get_level(IO_AR4);
-    printf("Right side: %d\n", rightside);
-
-
     gpio_set_direction(IO_AR5, GPIO_DIR_INPUT);
-    int leftside = gpio_get_level(IO_AR5);
-    printf("Left side: %d\n", leftside);
+    int rightside = gpio_get_level(IO_AR5);
+    // printf("Right side: %d\n", rightside);
 
+    gpio_set_direction(IO_AR6, GPIO_DIR_INPUT);
+    int leftside = gpio_get_level(IO_AR6);
+    // printf("Left side: %d\n", leftside);
 
-    sleep_msec(25);
-
-    if (rightside > 0 && leftside > 0) return 2;
-    else if (rightside > 0) return 1;
-    else if (leftside > 0) return 0;
-    else return -1;
+    if (rightside == 1 && leftside == 1) return IR_BOTH;
+    else if (rightside == 1) return IR_RIGHT;
+    else if (leftside == 1) return IR_LEFT;
+    else return IR_NONE;
 }
